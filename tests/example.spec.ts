51b3e79dba7d1fbe5dad896bbf2a8d81794e0c44
PrasadBQA <PrasadBQA@users.noreply.github.com>
2026-09-20 20:22:48 +0530

    Add Playwright multiple contexts examples

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,6 +1,12 @@
 import { test, expect } from '@playwright/test';
 
-test('has title', async ({ page }) => {
+test('has title1', async ({ page }) => {
+  await page.goto('https://playwright.dev/');
+
+  // Expect a title "to contain" a substring.
+  await expect(page).toHaveTitle(/Playwright/);
+});
+test('has title2', async ({ page }) => {
   await page.goto('https://playwright.dev/');
 
   // Expect a title "to contain" a substring.
@@ -19,3 +25,5 @@ test('get started link', async ({ page }) => {
 
   
 });
+
+// here we have 3 contexts, 3 pages -> 1 page for each context
---
+++ tests/normal-pw-page.ts
@@ -0,0 +1,19 @@
+import { chromium, Browser, BrowserContext, Page } from "playwright";
+
+async function run() {
+
+    let browser: Browser = await chromium.launch({headless: false});
+    let context: BrowserContext = await browser.newContext();
+    let page = await context.newPage();
+    
+    await page.goto("https://example.com");
+    console.log("Ttitle: ", await page.title());
+
+
+    // cleanup - reverse order
+    await page.close();
+    await context.close();
+    await browser.close();
+}
+
+run();
