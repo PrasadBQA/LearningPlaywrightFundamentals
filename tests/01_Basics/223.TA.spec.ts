a09e4649695a8de277c299c0e6db260f06bccd40
PrasadBQA <PrasadBQA@users.noreply.github.com>
2026-09-21 23:24:07 +0530

    Add Playwright context option examples

--- tests/01_Basics/223.TA.spec.ts
+++ tests/01_Basics/223.TA.spec.ts
@@ -4,3 +4,31 @@ import {test, expect} from '@playwright/test';
 // @playwright/test is Playwright's testing package.
 // The imported "test" is not a test itself, it's used to create one
 
+test('Navigating to the URL', async ({page}) => {
+    await page.goto("https://app.thetestingacademy.com/playwright/");
+});
+
+test('BCP - in app.vwo.com two roles', async({ browser }) => {
+
+    let adminContext = await browser.newContext();
+    let userContext = await browser.newContext();
+    let guestContext = await browser.newContext();
+
+    let adminPage = await adminContext.newPage();
+    await adminPage.goto("https://app.thetestingacademy.com/playwright/");
+
+
+    let userPage = await userContext.newPage();
+    await userPage.goto("https://sdet.live");
+
+
+    let guestPage = await guestContext.newPage();
+    await guestPage.goto("https://scrolltest.com");
+
+
+    await adminPage.close();
+    await userPage.close();
+    await guestPage.close();
+
+
+});
